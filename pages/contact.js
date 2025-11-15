9e80378e85bfc912c070164d80826540e010126b
bereketmulat <bereketmulat59@gmail.com>
2025-11-15 13:05:03 -0500

    Final

---
+++ pages/contact.js
@@ -0,0 +1,29 @@
+
+
+import Link from "next/link";
+export default function Contact(){
+    return (
+
+    <div className="Contact">
+
+
+        
+
+        <div className="ContactInfo">
+            <p>Contact Information</p>
+            <a href= "https://www.linkedin.com/in/bereket-mulat-mengiste-b10156272/">LinkedIn</a>
+            <p>Email: bereketmulat89@gmail.com</p>
+            <p>Phone Number: 226-501-4337</p>
+            <Link href="/">Back</Link>
+           
+
+        </div>
+
+       
+
+    </div>
+
+        
+    )
+
+}
---
+++ pages/projects.js
@@ -0,0 +1,19 @@
+
+import Link from "next/link";
+export default function projects(){
+    return (
+
+<div className="project">
+    <a href = "https://www.figma.com/proto/OcNAlL2jYjqRaOlFqSwvoq/Assignment-1-and-2-GUI-year-3-sem-1?node-id=722-640&t=QKhZEFwZSXYoL0se-1">Night Shift - Street Wear - Figma Project</a>
+    <a href = "https://github.com/bereketmulat/project1">Jack Pot - Lottery </a>
+    <Link href="/">Back</Link>
+   
+    
+
+       
+
+</div>
+        
+
+    )
+}
